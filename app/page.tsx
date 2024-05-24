3cf3d753da34e2ffaa97485c3865489a0259a050
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-05-24 19:02:45 +0900

    fix: turn <img> into <Image/>

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Spiner from "@/components/Spiner";
 import ShortcutList from "@/components/ShortcutList";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -7,11 +8,7 @@ export default function Home() {
       <Spiner width="50rem" height="50rem" circleNumber={10} />
       <section className="grid justify-items-center gap-2 p-2 bg-red-800 bg-opacity-50">
         <h1 className="fixed font-bold text-xs">Yooü m@ke 3y lif31up!</h1>
-        <img
-          className="w-28 h-32"
-          src="https://orig00.deviantart.net/667e/f/2015/225/c/b/black_cat_pixel_animation_by_sillybluewolf-d95it6h.png"
-          alt=""
-        />
+        <Image width={108} height={92} src="/cat.gif" alt="" />
         <ShortcutList />
       </section>
     </main>
