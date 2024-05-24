78d8773a0a162d07153ae4ba5d6867022a05473b
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-05-25 00:36:53 +0900

    add: bandcamp shortcut

--- components/ShortcutList.tsx
+++ components/ShortcutList.tsx
@@ -12,17 +12,14 @@ function ShortcutList({}) {
       <div className="grid gap-1 w-56">
         <Shortcut
           text="SoundCloud"
-          href="https://l.instagram.com/?u=https%3A%2F%2Fon.soundcloud.com%2FS5Gmy&e=AT348kw8t7POFzLpn2-XaBkJaSIqG_z6g1qT2hs_qlh9eUXrEfv1Ubes0Y_qDNKfpqoKxC2wT6bkLBxBUprX4_6xETVoOmDYG_PGUZfBmO_3PoG6mwz1lmQ"
-        />
-        <Shortcut
-          text="Pinterest"
-          href="https://l.instagram.com/?u=https%3A%2F%2Fwww.pinterest.co.kr%2Fifdefme%2F&e=AT3fz5fwlCTcOoONYJkNjASac-FjSjCYb_cY0kG2kpibonrhhuwggUBsP2nkuK5LhCQ6Y2lI6w6-WrMBra-GUk1ywEEbfg8EhlRrEHIlCsj6u44kzGs8kJg"
+          href="https://on.soundcloud.com/fuMxVfuHTdSG88EN9"
         />
         <Shortcut
           text="Spotify"
-          href="https://l.instagram.com/?u=https%3A%2F%2Fopen.spotify.com%2Fuser%2Fpcsyqo1bjc3yqbgd0rvwwske2%3Fsi%3DiZtDohlKRsi5WWu_TKeZSA&e=AT0d_xYA25gnjJpNO9lZO2NYGY8xRmNAcTHKk1S4EFIpJ7VKuawHyZfAOop39X1eHyiQ9Y6TqpXphDOg191NaOoUak0FHA3R"
+          href="https://open.spotify.com/user/pcsyqo1bjc3yqbgd0rvwwske2?si=iZtDohlKRsi5WWu_TKeZSA&nd=1&dlsi=f7bede0a46424145"
         />
         <Shortcut text="Instagram" href="https://www.instagram.com/lif31up" />
+        <Shortcut text="BandCamp" href="https://bandcamp.com/solve_anything" />
       </div>
     </div>
   );
